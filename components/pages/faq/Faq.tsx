d2c975b07d840d63b91f2564a7ac02e7e8880471
jewelrana3 <juyelrana7752@gmail.com>
2025-09-09 18:03:00 +0600

    faq complete

--- components/pages/faq/Faq.tsx
+++ components/pages/faq/Faq.tsx
@@ -1,9 +1,11 @@
 import ProfileSection from "@/components/share/ProfileSection";
 import React from "react";
+import FaqBody from "./FaqBody";
 
 export default function Faq() {
   return (
     <div>
+      <FaqBody />
       <ProfileSection />
     </div>
   );
--- components/share/SectionTitle.tsx
+++ components/share/SectionTitle.tsx
@@ -10,7 +10,9 @@ export default function SectionTitle({
   return (
     <div className="text-center p-12 bg-[#E9E9E9]">
       <h2 className="text-2xl text-[#F05223] font-bold">{title}</h2>
-      <p className="text-[#3D454E] text-2xl   mt-2">{subTitle}</p>
+      <p className="text-[#3D454E] text-2xl  mt-2 mx-auto lg:w-[37%]">
+        {subTitle}
+      </p>
     </div>
   );
 }
