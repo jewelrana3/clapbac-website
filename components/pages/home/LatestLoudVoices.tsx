cce5d1266474ca82ddfd400f0c77a92a9bf6c2ab
jewelrana3 <juyelrana7752@gmail.com>
2025-09-10 14:47:14 +0600

    working latest load

--- components/pages/home/LatestLoudVoices.tsx
+++ components/pages/home/LatestLoudVoices.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import CarouselPage from "./Carusel";
 
 export default function LatestLoudVoices() {
   return (
     <div>
+      <CarouselPage />
       <div className="bg-[#E9E9E9] px-10 py-20 sm:text-2xl font-bold text-center">
         <p>
           One-star warriors, keyboard critics — your spotlight’s on. <br /> We
