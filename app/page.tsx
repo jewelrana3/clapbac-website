1185e006ea4bc6cb3e3aa1884293005b0187cbc5
jewelrana3 <juyelrana7752@gmail.com>
2025-09-10 12:16:16 +0600

    home page compete

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
+import AboutFooter from "@/components/pages/about/AboutFooter";
+import FeatureBusiness from "@/components/pages/home/FeatureBussiness";
+import HeroSection from "@/components/pages/home/HeroSection";
+import LatestLoudVoices from "@/components/pages/home/LatestLoudVoices";
 import React from "react";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="mt-">
+      <HeroSection />
+      <LatestLoudVoices />
+      <FeatureBusiness />
+      <AboutFooter />
+    </div>
+  );
 }
---
+++ components/pages/home/LatestLoudVoices.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function LatestLoudVoices() {
+  return (
+    <div>
+      <div className="bg-[#E9E9E9] px-10 py-20 sm:text-2xl font-bold text-center">
+        <p>
+          One-star warriors, keyboard critics — your spotlight’s on. <br /> We
+          don’t just read reviews, we call it out, we clap back. <br /> Loudly.
+          Fairly. No Filters. No Passes.
+        </p>
+      </div>
+    </div>
+  );
+}
