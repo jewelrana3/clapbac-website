83cf879db51eed17fa12a8ccac2a13ee167a75b0
jewelrana3 <juyelrana7752@gmail.com>
2025-12-23 16:19:38 +0600

    image issue done

--- next.config.ts
+++ next.config.ts
@@ -4,14 +4,6 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["10.10.7.7", "82.180.137.106"],
   },
-  /* config options here */
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
-    return config;
-  },
 };
 
 export default nextConfig;
