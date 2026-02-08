49b7e83e694eba6bf5c554e0bb13fb2c09999b53
Rahad Ullah <rahadullah10@gmail.com>
2026-02-08 12:23:30 +0600

    fix(home-page): hero section video width issue

--- app/(website)/page.tsx
+++ app/(website)/page.tsx
@@ -4,8 +4,6 @@ import FeatureBusiness from "@/components/pages/home/FeatureBussiness";
 import HeroSection from "@/components/pages/home/HeroSection";
 import LatestLoudVoices from "@/components/pages/home/LatestLoudVoices";
 
-import React from "react";
-
 export default function Home() {
   return (
     <div className="">
--- components/pages/home/HeroSection.tsx
+++ components/pages/home/HeroSection.tsx
@@ -4,37 +4,15 @@ import Image from "next/image";
 export default async function HeroSection() {
   return (
     <>
-      <div
-        className="relative w-full bg-[#FEE3B3] h-[calc(100vh-80px)]"
-        style={{}}
-      >
-        {/* Background Image */}
-        {/* <Image
-          src={bgImage}
-          alt="Restaurant Background"
-          layout="fill"
-          objectFit="cover"
-          objectPosition="center"
-          priority
-        /> */}
-
+      <div>
         <video
-          src="/banner-two.mp4"
+          src="/banner.mp4"
           autoPlay
           loop
           muted
           playsInline
-          className="absolute inset-0 w-full mx-auto h-full object-contain ring-0 outline-none border border-[#FEE3B3]"
+          className="w-full max-w-[2560px] mx-auto"
         ></video>
-
-        {/* Overlay */}
-        {/* <div className="absolute inset-0  bg-opacity-40 flex flex-col items-center justify-center text-white px-4">
-          <h1 className="text-3xl md:text-5xl font-semibold text-center mb-6">
-            They rated you – now rate them back.
-          </h1>
-
-          <Search />
-        </div> */}
       </div>
       <section className="bg-black">
         <div className="flex flex-col lg:flex-row items-center justify-around py-12 px-8 gap-16">
